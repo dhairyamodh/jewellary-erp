6009cd218167f9a73cbced359ad3ee0e1c53cc66
Dhairya Modh <modhdhairya@gmail.com>
2024-02-17 13:31:44 +0530

    fix: remove get loan details

--- src/content/applications/Loan/AddLoanAmount.js
+++ src/content/applications/Loan/AddLoanAmount.js
@@ -61,7 +61,6 @@ const AddLoanAmount = () => {
         })
       );
       if (res?.payload.data.success) {
-        dispatch(updateInterestAsync({ id }));
         reset();
       }
       setPaymentLoading(false);
